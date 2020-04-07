c2bd1318fa1fc5b000f16d62649815c189e0709f
soumya ranjan <soumya.ranjan@gramener.com>
2020-04-07 22:01:41 +0530

    Fix framerate issue in ffmpeg

--- code/gif2mp4.py
+++ code/gif2mp4.py
@@ -7,7 +7,7 @@ def transform(path: str):
     folder = Path(path)
     for gif in folder.glob('*.gif'):
         subprocess.call(
-            f'ffmpeg -i {folder/gif.stem}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {folder/gif.stem}.mp4', shell=True)
+            f'ffmpeg -i {folder/gif.stem}.gif -vsync 0 -f mp4 -pix_fmt yuv420p -y {folder/gif.stem}.mp4', shell=True)
 
 
 if __name__ == '__main__':
